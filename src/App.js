2c758942fe9cae28a346f66dd9f31cc12b664734
ketakishore <ketakishore@gmail.com>
2024-02-28 05:03:59 +0000

    Assignmentproject

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Sidenavbar from "./components/Sidenavbar";
+import Bid from "./components/Bid";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <Header />
+      <div className="bid-container">
+        <Sidenavbar />
+        <Bid />
+      </div>
     </div>
   );
 }
